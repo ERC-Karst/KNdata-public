ed7bedab17f49ff85fd052c0e6e056f3661f6e16
Celia Trunz <30932911+cctrunz@users.noreply.github.com>
2025-11-12 12:54:28 +0100

    update and modify name convention for full datasets to 000

--- find_your_cave.py
+++ find_your_cave.py
@@ -3,10 +3,16 @@ import pandas as pd
 
 #%%
 
-df = pd.read_csv('caves_properties_summary_all_individual.csv',sep=';')
+df = pd.read_csv('C:/Users/celia/github/erc-karst-repositories/KNdata-public/caves_properties_summary_all_individual.csv',sep=';')
+# %%
+geometry = df.percentage_csdim > 80
+# mean_degree = df.mean_degree > 2.2
+length = df.total_length > 100000
+
+result = df[length][['id_dataset','short_name','id_subset','total_length','mean_degree','percentage_csdim']].sort_values(by=['total_length','percentage_csdim'],ascending=False)
+print(result)
+result.to_csv('selected_caves.csv')
+
 # %%
 
-df[["mean_degree", "percentage_csdim"]].max()
-# %%
-high_degree = df[df.mean_degree > 2]
 # %%
